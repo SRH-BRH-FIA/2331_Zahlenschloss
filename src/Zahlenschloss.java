f189939fce50c14870a01d9278289628e1ab6308
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-05-22 14:35:10 +0200

    Schwierigkeitsgrad eingebaut, und entsprechende Methoden angepasst.

--- src/Zahlenschloss.java
+++ src/Zahlenschloss.java
@@ -14,10 +14,29 @@ public class Zahlenschloss {
         System.out.println( geheimeZahlIstInOrdnung(120) );
         */
 
-        int geheimZahl = findeZufälligeZahl();
-        int anzahlVersuche = 0;
-
         Scanner eingabe = new Scanner(System.in);
+
+        char schwierigkeitsgrad = 'L';
+        System.out.println("Mastermind mit Zahlen! v1.0");
+        do {
+            System.out.print("Bitte wählen Sie Ihren Schwierigkeitsgrad (L, M, S): ");
+            schwierigkeitsgrad = eingabe.nextLine().toUpperCase().charAt(0);
+        } while (schwierigkeitsgrad != 'L' & schwierigkeitsgrad != 'M' & schwierigkeitsgrad != 'S');
+
+        int anzahlZiffern = 3;
+        if (schwierigkeitsgrad == 'L') {
+            anzahlZiffern = 3;
+        }
+        else if (schwierigkeitsgrad == 'M') {
+            anzahlZiffern = 5;
+        }
+        else {
+            anzahlZiffern = 7;
+        }
+
+        int anzahlVersuche = 0;
+        int geheimZahl = findeZufälligeZahl(anzahlZiffern);
+
         while (true) {
             System.out.print("Bitte Tipp für Geheimzahl eingeben (0 für Abbruch): ");
             int zahl = eingabe.nextInt();
@@ -70,22 +89,24 @@ public class Zahlenschloss {
         return false;
     }
 
-    private static int findeZufälligeZahl() {
+    private static int findeZufälligeZahl(int anzahlZiffern) {
         int geheimeZahl;
+        int startwert    = (int)Math.pow(10, anzahlZiffern-1);
+        int wertebereich = (int)Math.pow(10, anzahlZiffern) - startwert;
         do {
-            geheimeZahl = (int) (Math.random() * 900 + 100);
-        } while ( ! geheimeZahlIstInOrdnung(geheimeZahl) );
+            geheimeZahl = (int) (Math.random() * wertebereich + startwert);
+        } while ( ! geheimeZahlIstInOrdnung(geheimeZahl, anzahlZiffern) );
         return geheimeZahl;
     }
 
-    private static boolean geheimeZahlIstInOrdnung(int geheimeZahl) {
+    private static boolean geheimeZahlIstInOrdnung(int geheimeZahl, int anzahlZiffern) {
         int anzahlUnterschiedlicherZiffern = 0;
         for (int ziffer=0; ziffer<=9; ziffer++) {
             if ( zifferInGeheimerZahlVorhanden(ziffer, geheimeZahl) ) {
                 anzahlUnterschiedlicherZiffern++;
             }
         }
-        return (anzahlUnterschiedlicherZiffern == 3);
+        return (anzahlUnterschiedlicherZiffern == anzahlZiffern);
     }
 
     private static void meldeAnzahlVersuche(int anzahlVersuche) {
